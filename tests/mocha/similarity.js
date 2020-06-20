bb93798e72a0cb2d5ecd6f32dcd55d5f22b17c82
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-19 23:58:36 -0400

    removes length assumption

--- tests/mocha/similarity.js
+++ tests/mocha/similarity.js
@@ -3,8 +3,7 @@
 module.exports = function (buffer1, buffer2) {
   const hexBuffer1 = buffer1.toString('hex')
   const hexBuffer2 = buffer2.toString('hex')
-  const length = hexBuffer1.length
-  // assuming both hash have the same length
+  const length = Math.min(hexBuffer1.length, hexBuffer2.length)
   const match = hexBuffer1.split('').reduce((count, value, index) => {
     if (value === hexBuffer2[index]) {
       return count + 1
