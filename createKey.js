1809751dd96f1a92e8d525e4678330b7a7d35593
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-25 22:20:54 -0400

    Exposes offset

--- createKey.js
+++ createKey.js
@@ -8,20 +8,21 @@ function allocate (key, salt) {
   const hash = crypto.createHash('sha512')
   // hash is 64 bytes long, pad content to minimize the repetition
   const lengthMod64 = key.length % 64
-  let paddingLength
+  let offset
   if (lengthMod64 === 0) {
-    paddingLenth = 63
+    offset = 63
   } else {
-    paddingLength = 65 - lengthMod64
+    offset = 65 - lengthMod64
   }
-  const saltedKey = Buffer.allocUnsafe(key.length + paddingLength)
+  const saltedKey = Buffer.allocUnsafe(key.length + offset)
   key.copy(saltedKey)
-  salt.copy(saltedKey, key.length, 0, paddingLength)
+  salt.copy(saltedKey, key.length, 0, offset)
   hash.update(saltedKey)
   return {
     saltedKey,
     hash: hash.digest(),
-    salt
+    salt,
+    offset
   }
 }
 
