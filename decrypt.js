ad3cb471176b3fd3d83cb0a668b42bb1694bbc14
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-25 22:23:00 -0400

    wip

--- decrypt.js
+++ decrypt.js
@@ -1,1 +1,60 @@
-module.exports = require('./encrypt')
+'use strict'
+
+const CryptoStream = require('./CryptoStream')
+const createKey = require('./createKey')
+const mask = require('./mask')
+const toBuffer = require('./toBuffer')
+
+async function decrypt (key, buffer) {
+  return toBuffer(await decrypt.createStream(key), buffer)
+}
+
+class DecryptionStream extends CryptoStream {
+  _read () {
+    let count
+    while (this._chunks.length) {
+      ++count
+      if (!this.push(this._chunks.shift())) {
+        break
+      }
+    }
+    this._pending = !count
+  }
+
+  _readIfPending () {
+    if (this._pending) {
+      this._read()
+    }
+  }
+
+  _write (chunk, encoding, onwrite) {
+    const encrypted = Buffer.from(chunk)
+    for (let index = 0; index < chunk.length; ++index) {
+      const byteMask = mask(this._key, this._offset++)
+      encrypted[index] = encrypted[index] ^ byteMask
+    }
+    this._chunks.push(encrypted)
+    this._readIfPending()
+    onwrite()
+  }
+
+  end () {
+    this._chunks.push(null)
+    this._readIfPending()
+    return super.end.apply(this, arguments)
+  }
+
+  constructor (options) {
+    super(options)
+    this._offset = 0
+    this._chunks = []
+  }
+}
+
+decrypt.createStream = async function (key) {
+  const stream = new EncryptionStream()
+  stream._key = await createKey(key)
+  return stream
+}
+
+module.exports = decrypt
