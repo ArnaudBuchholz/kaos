6223d44af690613290a04428b94b611834820b4c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-26 23:46:34 -0400

    Do not expose createKey

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 'use strict'
 
-const createKey = require('./createKey.js')
 const encrypt = require('./encrypt.js')
 const decrypt = require('./decrypt.js')
 
 module.exports = {
-  createKey,
   encrypt,
   decrypt
 }
