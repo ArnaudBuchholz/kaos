ff3cc5523d14a63afbf2b2d1c6b399fd2254b577
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-28 23:40:50 -0400

    Test streaming and measure performance

--- tests/mocha/decrypt.test.js
+++ tests/mocha/decrypt.test.js
@@ -3,16 +3,18 @@
 const assert = require('assert')
 const encrypt = require('../../encrypt')
 const decrypt = require('../../decrypt')
+const toBuffer = require('../../toBuffer')
 const similarity = require('./similarity')
 
 const secretKey = 'my secret key'
+const messageToEncrypt = 'Hello World !'
 
 describe('decrypt', () => {
   let message
   let encrypted
 
   before(async () => {
-    message = Buffer.from('Hello World !', 'utf8')
+    message = Buffer.from(messageToEncrypt, 'utf8')
     encrypted = await encrypt(secretKey, message)
   })
 
@@ -21,15 +23,45 @@ describe('decrypt', () => {
     assert.strictEqual(message.length, decrypted.length)
     assert.strictEqual(similarity(message, decrypted), 100)
   })
-/*
-  it('supports partial decryption', async () => {
-    const concatenated = Buffer.alloc(encrypted.length)
-    for (let index = 0; index < encrypted.length; ++index) {
-      const partial = Buffer.alloc(1, encrypted[index])
-      decrypt(key, partial, index)
-      concatenated[index] = partial[0]
+
+  it('supports streaming', async () => {
+    const stream = await decrypt.createStream(secretKey)
+    const promise = toBuffer(stream).then(buffer => {
+      const decryptedMessage = buffer.toString('utf8')
+      assert.strictEqual(decryptedMessage, messageToEncrypt)
+    })
+    let index = 0
+    function next () {
+      if (index < encrypted.length) {
+        stream.write(Buffer.alloc(1, encrypted[index]), next)
+        ++index
+      } else {
+        stream.end()
+      }
     }
-    assert.ok(similarity(message, concatenated) === 100)
+    next()
+    return promise
   })
-*/
+
+  describe('performance', () => {
+    let bigMessage
+    let bigEncrypted
+
+    before(async () => {
+      bigMessage = Buffer.allocUnsafe(1024 * 1024 * 10)
+      bigEncrypted = await encrypt(secretKey, bigMessage)
+    })
+
+    for (var count = 0; count < 10; ++count) {
+      it(`is performant (round ${count + 1})`, async () => {
+        const start = process.hrtime()
+        const decrypted = await decrypt(secretKey, bigEncrypted)
+        const duration = process.hrtime(start)
+        const ms = duration[1] / 1000000
+        const speed = Math.floor(bigMessage.length / (1024 * ms))
+        console.info('        Execution time %dms, speed %d Kb/ms', Math.floor(ms), speed)
+        assert.strictEqual(similarity(decrypted, bigMessage), 100)
+      })
+    }
+  })
 })
