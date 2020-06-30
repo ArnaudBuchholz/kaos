aa7de9f676bafe9eb4e598e2370ec17d6b2f9c8a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-29 22:11:55 -0400

    Command line implementation

--- index.js
+++ index.js
@@ -3,7 +3,36 @@
 const encrypt = require('./encrypt.js')
 const decrypt = require('./decrypt.js')
 
-module.exports = {
-  encrypt,
-  decrypt
+async function pipe ({ createStream, source, target, key }) {
+  const { createReadStream, createWriteStream, stat } = require('fs')
+  const statAsync = require('util').promisify(stat)
+  let resolvedKey
+  try {
+    const stats = await statAsync(key)
+    if (stats.isFile()) {
+      resolvedKey = createReadStream(key)
+    }
+  } catch (e) {
+    resolvedKey = key
+  }
+  const processor = await createStream(resolvedKey)
+  createReadStream(source).pipe(processor)
+  processor.pipe(createWriteStream(target))
+}
+
+if (require.main === module) {
+  const [,, mode, source, target, key] = process.argv
+  if (!'e,d,encrypt,decrypt'.includes(mode)) {
+    throw new Error('Invalid mode')
+  }
+  if (mode.charAt(0) === 'd') {
+    pipe({ createStream: decrypt.createStream, source, target, key })
+  } else {
+    pipe({ createStream: encrypt.createStream, source, target, key })
+  }
+} else {
+  module.exports = {
+    encrypt,
+    decrypt
+  }
 }
