8dade5e1747c59ed11f7642e69a8279d40d153d4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-03 23:45:17 -0400

    Exposes offset computation

--- createKey.js
+++ createKey.js
@@ -4,19 +4,38 @@ const crypto = require('crypto')
 const { Readable } = require('stream')
 const toBuffer = require('./toBuffer')
 
-function allocate (key, salt) {
-  const hash = crypto.createHash('sha512')
+async function keyToBuffer (key) {
+  if (key instanceof Buffer) {
+    return key
+  }
+  if (typeof key === 'string') {
+    return Buffer.from(key)
+  }
+  if (key instanceof Readable) {
+    return await toBuffer(key)
+  }
+  throw new Error('Unsupported key type')
+}
+
+async function getBufferAndOffset (key) {
+  const buffer = await keyToBuffer(key)
   // hash is 64 bytes long, pad content to minimize the repetition
-  const lengthMod64 = key.length % 64
+  const lengthMod64 = buffer.length % 64
   let offset
   if (lengthMod64 < 32) {
     offset = 63 - lengthMod64 /* 63, 62, ..., 32 */
   } else {
     offset = 1 + lengthMod64 /* 33, 34, ..., 63 */
   }
-  const saltedKey = Buffer.allocUnsafe(key.length + offset)
-  key.copy(saltedKey)
-  salt.copy(saltedKey, key.length, 0, offset)
+  return { buffer, offset }
+}
+
+async function createKey (key, salt = crypto.randomBytes(64)) {
+  const { buffer, offset } = await getBufferAndOffset(key)
+  const hash = crypto.createHash('sha512')
+  const saltedKey = Buffer.allocUnsafe(buffer.length + offset)
+  buffer.copy(saltedKey)
+  salt.copy(saltedKey, buffer.length, 0, offset)
   hash.update(saltedKey)
   return {
     saltedKey,
@@ -26,15 +45,6 @@ function allocate (key, salt) {
   }
 }
 
-module.exports = async function createKey (key, salt = crypto.randomBytes(64)) {
-  if (typeof key === 'string') {
-    return allocate(Buffer.from(key), salt)
-  }
-  if (key instanceof Buffer) {
-    return allocate(key, salt)
-  }
-  if (key instanceof Readable) {
-    return allocate(await toBuffer(key), salt)
-  }
-  throw new Error('Unsupported parameter')
-}
+createKey.getBufferAndOffset = getBufferAndOffset
+
+module.exports = createKey
