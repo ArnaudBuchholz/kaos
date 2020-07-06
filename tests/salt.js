d290166029370a8411756ad05fb155c521fb8317
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-05 23:07:09 -0400

    Trying to understand impact of salt on similarity

---
+++ tests/salt.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const crypto = require('crypto')
+const encrypt = require('../encrypt')
+const toBuffer = require('../toBuffer')
+const similarity = require('./mocha/similarity')
+
+const secretKey = 'my secret key'
+const message = Buffer.from('Hello World !', 'utf8')
+
+const salt = Buffer.alloc(64)
+
+async function main () {
+  for (let byte = 0; byte < 256; ++byte) {
+    salt.fill(byte)
+    const encrypted = await encrypt(secretKey, message, salt)
+    console.log(byte, similarity(message, encrypted))
+  }
+}
+
+main()
