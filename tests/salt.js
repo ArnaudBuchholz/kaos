e9c83b79ed755b5fdef47ecfc25183369406a643
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-05 23:30:46 -0400

    Might have a bug in similarity

--- tests/salt.js
+++ tests/salt.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const encrypt = require('../encrypt')
+const createKey = require('../createKey')
 const similarity = require('./mocha/similarity')
 
 const secretKey = 'my secret key'
@@ -9,10 +10,13 @@ const message = Buffer.from('Hello World !', 'utf8')
 const salt = Buffer.alloc(64)
 
 async function main () {
+  console.log(message.toString('hex'))
   for (let byte = 0; byte < 256; ++byte) {
     salt.fill(byte)
     const encrypted = await encrypt(secretKey, message, salt)
-    console.log(byte, similarity(message, encrypted))
+    const key = await createKey(secretKey, salt)
+    const comparable = encrypted.slice(key.offset)
+    console.log(byte, similarity(message, comparable), comparable.toString('hex'))
   }
 }
 
