ce8094be7bb404144f9f996c43ae79c08c8f45de
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-07 23:22:11 -0400

    Ensures mask is never 0

--- mask.js
+++ mask.js
@@ -5,5 +5,15 @@ function get (buffer, offset) {
 }
 
 module.exports = (key, offset) => {
-  return get(key.saltedKey, offset) ^ get(key.hash, offset)
+  const keyByte = get(key.saltedKey, offset)
+  const hashByte = get(key.hash, offset)
+  const xor = keyByte ^ hashByte
+  if (xor !== 0) {
+    return xor
+  }
+  const or = keyByte | hashByte
+  if (or !== 0) {
+    return or
+  }
+  return keyByte || hashByte || 85
 }
