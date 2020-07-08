76c6eb61a5d13b98e6c62684568df0067c2a6ab1
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-07 23:22:41 -0400

    Similarity compares bytes

--- tests/similarity.js
+++ tests/similarity.js
@@ -3,12 +3,10 @@
 const assert = require('assert')
 
 module.exports = function (buffer1, buffer2, expectedMin = undefined) {
-  const hexBuffer1 = buffer1.toString('hex')
-  const hexBuffer2 = buffer2.toString('hex')
-  const length = Math.min(hexBuffer1.length, hexBuffer2.length)
+  const length = Math.min(buffer1.length, buffer2.length)
   const match = []
   for (let index = 0; index < length; ++index) {
-    match.push(hexBuffer1[index] === hexBuffer2[index])
+    match.push(buffer1[index] === buffer2[index])
   }
   match.count = match.reduce((total, matched) => {
     if (matched) {
@@ -16,8 +14,8 @@ module.exports = function (buffer1, buffer2, expectedMin = undefined) {
     }
     return total
   }, 0)
-  match.percent = Math.floor(100 * match.count / length) // %
-  if (expectedMin && match.percent > expectedMin) {
+  match.percent = Math.ceil(100 * match.count / length) // %
+  if (expectedMin !== undefined && match.percent > expectedMin) {
     assert(false, `Similarity ${match.percent}% too high, expected ${expectedMin}%`)
   }
   return match
