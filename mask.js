22180b6ad01db7e7cb3046d1fa1878d67ec42b1c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-08 21:17:29 -0400

    Shorter code

--- mask.js
+++ mask.js
@@ -1,19 +1,9 @@
 'use strict'
 
-function get (buffer, offset) {
-  return buffer[offset % buffer.length]
-}
+const get = (buffer, offset) => buffer[offset % buffer.length]
 
 module.exports = (key, offset) => {
   const keyByte = get(key.saltedKey, offset)
   const hashByte = get(key.hash, offset)
-  const xor = keyByte ^ hashByte
-  if (xor !== 0) {
-    return xor
-  }
-  const or = keyByte | hashByte
-  if (or !== 0) {
-    return or
-  }
-  return keyByte || hashByte || 85
+  return keyByte ^ hashByte || keyByte | hashByte || keyByte || hashByte || 85
 }
