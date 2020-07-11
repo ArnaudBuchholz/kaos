28543f4761d197d16dd82d16d4d9a1f84afee24a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-10 22:02:33 -0400

    Key handler

--- key.js
+++ key.js
@@ -4,14 +4,7 @@ const crypto = require('crypto')
 const { Readable } = require('stream')
 const toBuffer = require('./toBuffer')
 
-class Key {
-
-  constructor (source, salt) {
-
-  }
-}
-
-async function sourceToBuffer (source) {
+async function getBuffer (source, sourceType) {
   if (source instanceof Buffer) {
     return source
   }
@@ -21,30 +14,64 @@ async function sourceToBuffer (source) {
   if (source instanceof Readable) {
     return await toBuffer(source)
   }
-  throw new Error('Unsupported key source')
+  throw new Error(`Unsupported ${sourceType} type`)
 }
 
-async function computeSaltLength (buffer) {
-  // hash is 64 bytes long, pad content to minimize the repetition
-  const lengthMod64 = buffer.length % 64
-  let saltLength
-  if (lengthMod64 < 32) {
-    return saltLength = 63 - lengthMod64 /* 63, 62, ..., 32 */
+class Key {
+  constructor (source) {
+    this._source = source
   }
-  return saltLength = 1 + lengthMod64 /* 33, 34, ..., 63 */
-}
 
-module.exports = (source, salt) => new Key(source, salt)
-
-/*
-  const { buffer, saltLength } = await getBufferAndSaltLength(key)
-  const saltedKey = Buffer.allocUnsafe(buffer.length + saltLength)
-  const key {
-    saltedKey,
-    saltLength,
-
-    salt () {
-
+  async _getKey () {
+    if (!this._key) {
+      this._key = await getBuffer(this._source, 'key')
     }
+    return this._key
   }
-}*/
+
+  async _computeSaltLength () {
+    if (!this._saltLength) {
+      // hash is 64 bytes long, pad content to minimize the repetition
+      const lengthMod64 = (await this._getKey()).length % 64
+      if (lengthMod64 < 32) {
+        this._saltLength = 63 - lengthMod64 /* 63, 62, ..., 32 */
+      } else {
+        this._saltLength = 1 + lengthMod64 /* 33, 34, ..., 63 */
+      }
+    }
+    return this._saltLength
+  }
+
+  async salt (source = crypto.randomBytes(64)) {
+    const saltLength = await this._computeSaltLength()
+    this._salt = await getBuffer(source, 'salt')
+    if (this._salt.length < saltLength) {
+      throw new Error(`Salt too small (expected length is ${saltLength} bytes)`)
+    }
+    if (this._salt.length > saltLength) {
+      this._salt = this._salt.slice(0, saltLength)
+    }
+    const hash = crypto.createHash('sha512')
+    hash.update(this._key)
+    hash.update(this._salt)
+    this._hash = hash.digest()
+    this._saltedKeyLength = this._key.length + saltLength
+    this._initialOffset = this._salt.readUInt32BE(saltLength - 4)
+  }
+
+  mask (offset) {
+    const shiftedOffset = this._initialOffset + offset
+    const keyOffset = shiftedOffset % this._saltedKeyLength
+    const keyLength = this._key.length
+    let keyByte
+    if (keyOffset < keyLength) {
+      keyByte = this._key[keyOffset]
+    } else {
+      keyByte = this._salt[keyOffset - keyLength]
+    }
+    const hashByte = this._hash[shiftedOffset % this._hash.length]
+    return keyByte ^ hashByte || keyByte | hashByte || keyByte || hashByte || 85
+  }
+}
+
+module.exports = source => new Key(source)
