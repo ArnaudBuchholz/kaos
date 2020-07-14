1edbae93bd14b0588dcc268a4b21cd96962ae841
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-13 21:40:20 -0400

    Base Transform

---
+++ Transform.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const { Transform } = require('stream')
+const key = require('./key')
+
+module.exports = class KaosTransform extends Transform {
+  _mask (chunk) {
+    const buffer = Buffer.from(chunk)
+    const key = this._key
+    for (let index = 0; index < chunk.length; ++index) {
+      const byteMask = key.mask(this._offset++)
+      buffer[index] = buffer[index] ^ byteMask
+    }
+    this.push(buffer)
+  }
+
+  constructor (secretKey, options) {
+    super(options)
+    this._key = key(secretKey)
+  }
+}
