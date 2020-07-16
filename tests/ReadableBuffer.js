1933d85c40e34c79a2849d18693968896f443180
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-15 21:28:04 -0400

    Enables array of buffer

--- tests/ReadableBuffer.js
+++ tests/ReadableBuffer.js
@@ -4,7 +4,11 @@ const { Readable } = require('stream')
 
 module.exports = class ReadableBuffer extends Readable {
   _read () {
-    this.push(this._buffer)
+    if (Array.isArray(this._buffer)) {
+      this._buffer.forEach(buffer => this.push(buffer))
+    } else {
+      this.push(this._buffer)
+    }
     this.push(null)
   }
 
