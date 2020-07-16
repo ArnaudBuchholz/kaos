83696903d80150ed03e2a7a31e650f95f50e2ac8
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-15 21:27:31 -0400

    Fix when used with salted key

--- encrypt.js
+++ encrypt.js
@@ -6,6 +6,8 @@ class KaosEncrypt extends KaosTransform {
   async _transform (chunk, encoding, callback) {
     if (!this._key._salt) {
       this._key = await this._key.salt()
+    }
+    if (this._offset === undefined) {
       this._offset = this._key._initialOffset
       this.push(this._key._salt)
     }
