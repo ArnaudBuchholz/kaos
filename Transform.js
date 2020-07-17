caac82bdb5268b64a51c6bd786ca49321c524add
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-16 22:11:06 -0400

    mask is now in Transform for performance reasons

--- Transform.js
+++ Transform.js
@@ -5,19 +5,40 @@ const key = require('./key')
 
 module.exports = class KaosTransform extends Transform {
   _mask (chunk) {
-    ++this._nbCallsToMask
     const buffer = Buffer.from(chunk)
+    const length = chunk.length
     const key = this._key
-    for (let index = 0; index < chunk.length; ++index) {
-      const byteMask = key.mask(this._offset++)
+    const shiftedOffset = key._initialOffset + this._offset
+    const saltedKeyLength = key._saltedKeyLength
+    let keyOffset = shiftedOffset % saltedKeyLength
+    const hashLength = key._hash.length
+    let hashOffset = shiftedOffset % key._hash.length
+    const unsaltedKeyLength = key._key.length
+    let index = 0
+    while (length - index) {
+      let keyByte
+      if (keyOffset < unsaltedKeyLength) {
+        keyByte = key._key[keyOffset]
+      } else {
+        keyByte = key._salt[keyOffset - unsaltedKeyLength]
+      }
+      const hashByte = key._hash[hashOffset]
+      const byteMask = keyByte ^ hashByte || keyByte | hashByte || keyByte || hashByte || 85
       buffer[index] = buffer[index] ^ byteMask
+      if (++keyOffset >= saltedKeyLength) {
+        keyOffset = 0
+      }
+      if (++hashOffset >= hashLength) {
+        hashOffset = 0
+      }
+      ++index
     }
+    this._offset += length
     this.push(buffer)
   }
 
   constructor (secretKey, options) {
     super(options)
-    this._nbCallsToMask = 0
     this._key = key(secretKey)
   }
 }
