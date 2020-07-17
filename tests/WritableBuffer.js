52246657257c93c5d8fe5046c219c9be4c4b1027
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-16 22:11:30 -0400

    Count the number of writes to check streaming

--- tests/WritableBuffer.js
+++ tests/WritableBuffer.js
@@ -4,8 +4,13 @@ const { Writable } = require('stream')
 
 module.exports = class WritableBuffer extends Writable {
   _write (chunk, encoding, onwrite) {
+    ++this._writeCount
     this._buffer = Buffer.concat([this._buffer, chunk])
     onwrite()
+  }
+
+  get writeCount () {
+    return this._writeCount
   }
 
   get buffer () {
@@ -14,6 +19,7 @@ module.exports = class WritableBuffer extends Writable {
 
   constructor (buffer, options) {
     super(options)
+    this._writeCount = 0
     this._buffer = Buffer.from('')
   }
 }
