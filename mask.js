7a6761df1191f39d5f64a4f604a771a9bd1950f4
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-19 13:20:35 -0400

    Is within Transform

--- mask.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const get = (buffer, offset) => buffer[offset % buffer.length]
-
-module.exports = (key, offset) => {
-  const keyByte = get(key.saltedKey, offset)
-  const hashByte = get(key.hash, offset)
-  return keyByte ^ hashByte || keyByte | hashByte || keyByte || hashByte || 85
-}
