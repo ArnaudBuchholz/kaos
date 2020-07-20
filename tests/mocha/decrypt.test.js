e60388e9fdce495b1bef322693871b741b1c269e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-19 23:30:57 -0400

    wip

--- tests/mocha/decrypt.test.js
+++ tests/mocha/decrypt.test.js
@@ -16,9 +16,7 @@ describe('decrypt', () => {
   const unsaltedKey = key(literalKey)
   const message = Buffer.from('Hello World !', 'utf8')
   let saltedKey
-  let saltLength
   let encrypted
-  let encryptedWithoutSalt
 
   before(async () => {
     saltedKey = await unsaltedKey.salt()
@@ -29,7 +27,6 @@ describe('decrypt', () => {
       writable
     )
     encrypted = writable.buffer
-    saltLength = saltedKey._saltLength
   })
 
   it('decrypts the message (raw key)', async () => {
@@ -48,31 +45,55 @@ describe('decrypt', () => {
         assert.strictEqual(transform._key[property], saltedKey[property], property)
       }
     })
+    assert.strictEqual(writable.writeCount, 1 /* decrypted */)
     const decrypted = writable.buffer
     assert.strictEqual(message.length, decrypted.length)
     assert.strictEqual(similarity(message, decrypted).percent, 100)
   })
 
-/*
-  it('supports streaming', async () => {
-    const stream = await decrypt.createStream(secretKey)
-    const promise = toBuffer(stream).then(buffer => {
-      const decryptedMessage = buffer.toString('utf8')
-      assert.strictEqual(decryptedMessage, messageToEncrypt)
-    })
-    let index = 0
-    function next () {
-      if (index < encrypted.length) {
-        stream.write(Buffer.alloc(1, encrypted[index]), next)
-        ++index
+  const versions = {
+    unsalted: unsaltedKey,
+    salted: 0
+  }
+  Object.keys(versions).forEach(label => {
+    it(`supports ${label} key`, async () => {
+      let keyVersion
+      if (label === 'salted') {
+        keyVersion = saltedKey
       } else {
-        stream.end()
+        keyVersion = versions[label]
       }
-    }
-    next()
-    return promise
+      const writable = new WritableBuffer()
+      await pipeline(
+        new ReadableBuffer(encrypted),
+        decrypt(keyVersion),
+        writable
+      )
+      const result = writable.buffer
+      assert.strictEqual(message.length, result.length)
+      assert.strictEqual(similarity(message, result).percent, 100)
+    })
   })
 
+  it('supports streaming (each byte individually)', async () => {
+    const buffers = []
+    for (let offset = 0; offset < encrypted.length; ++offset) {
+      buffers.push(encrypted.slice(offset, offset + 1))
+    }
+    const writable = new WritableBuffer()
+    const transform = decrypt(literalKey)
+    await pipeline(
+      new ReadableBuffer(buffers),
+      transform,
+      writable
+    )
+    assert.strictEqual(writable.writeCount, message.length)
+    const result = writable.buffer
+    assert.strictEqual(message.length, result.length)
+    assert.strictEqual(similarity(message, result).percent, 100)
+  })
+
+  /*
   it('supports partial streaming', async () => {
     const info = await decrypt.getPartialStreamInfo(secretKey, 6, 11)
     assert.strictEqual(messageToEncrypt.substring(6, 11), 'World')
@@ -85,15 +106,22 @@ describe('decrypt', () => {
     stream.write(encrypted.slice(info.from, info.to), () => stream.end())
     return promise
   })
+*/
 
   describe('performance', function () {
-    this.timeout(10000)
+    this.timeout(0)
     let bigMessage
     let bigEncrypted
 
     before(async () => {
       bigMessage = Buffer.allocUnsafe(1024 * 1024 * 10)
-      bigEncrypted = await encrypt(secretKey, bigMessage)
+      const writable = new WritableBuffer()
+      await pipeline(
+        new ReadableBuffer(bigMessage),
+        encrypt(unsaltedKey),
+        writable
+      )
+      bigEncrypted = writable.buffer
     })
 
     const loops = 10
@@ -101,16 +129,22 @@ describe('decrypt', () => {
     it(`is performant (loops=${loops})`, async () => {
       let cumulated = 0
       for (var count = 0; count < loops; ++count) {
+        const writable = new WritableBuffer()
         const start = process.hrtime()
-        const decrypted = await decrypt(secretKey, bigEncrypted)
+        await pipeline(
+          new ReadableBuffer(bigEncrypted),
+          decrypt(unsaltedKey),
+          writable
+        )
         const duration = process.hrtime(start)
         cumulated += duration[1] / 1000000
-        assert.strictEqual(similarity(decrypted, bigMessage).percent, 100)
+        const result = writable.buffer
+        assert.strictEqual(bigMessage.length, result.length)
+        assert.strictEqual(similarity(bigMessage, result).percent, 100)
       }
       const ms = cumulated / loops
       const speed = Math.floor(bigMessage.length / (1024 * ms))
       console.info('        Execution time %dms, speed %d Kb/ms', Math.floor(ms), speed)
     })
   })
-*/
 })
