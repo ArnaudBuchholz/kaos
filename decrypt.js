f9b1bc3c096ee8eb81c3406ffff9e167cba88112
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-07-20 22:28:24 -0400

    Supports range

--- decrypt.js
+++ decrypt.js
@@ -15,7 +15,6 @@ class KaosDecrypt extends KaosTransform {
       this._saltOffset += lengthForSalt
       if (this._saltOffset === this._saltLength) {
         this._key = await this._key.salt(this._salt)
-        this._offset = 0
         if (lengthForSalt < chunk.length) {
           const chunkTail = chunk.subarray(lengthForSalt)
           this._mask(chunkTail)
@@ -27,36 +26,10 @@ class KaosDecrypt extends KaosTransform {
     callback()
   }
 
-  constructor (secretKey, options) {
+  constructor (secretKey, range = { start: 0 }, options) {
     super(secretKey, options)
-    delete this._key._salt
+    this._offset = range.start
   }
 }
 
-/*
-decrypt.createStream = async function (key) {
-  const { buffer, offset } = await createKey.getBufferAndOffset(key)
-  return new DecryptionStream(buffer, offset)
-}
-
-decrypt.getPartialStreamInfo = async function (key, from, to) {
-  const { buffer, offset } = await createKey.getBufferAndOffset(key)
-  from += offset
-  to += offset
-  return { key: buffer, offset, from, to }
-}
-
-decrypt.createPartialStream = async function (info, salt) {
-  const stream = new DecryptionStream()
-  const key = await createKey(info.key, salt)
-  stream._key = key
-  stream._offset = info.from - info.offset + key.salt.readUInt32BE(key.offset - 4)
-  return stream
-}
-*/
-
-function decrypt (key) {
-  return new KaosDecrypt(key)
-}
-
-module.exports = decrypt
+module.exports = (key, range) => new KaosDecrypt(key, range)
